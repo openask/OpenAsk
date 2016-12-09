24a963ce7b0f489ad3f21aa40ba475bbc8c28c76
upliu <i@upliu.net>
2016-12-09 21:54:16 +0800

    kill php 后 yii 的那个进程自动就结束了

--- stop-dev.sh
+++ stop-dev.sh
@@ -3,7 +3,7 @@
 RUNTIME_DIR=`pwd`/runtime
 
 # kill yii
-kill `ps -ef|grep './yii serve --docroot=@app/web --port=8080 127.0.0.1'|head -n1|awk '{print $2}'`
+# kill `ps -ef|grep './yii serve --docroot=@app/web --port=8080 127.0.0.1'|head -n1|awk '{print $2}'`
 # kill php
 kill `ps -ef|grep '\-S 127.0.0.1:8080 \-t'|head -n1|awk '{print $2}'`
 # kill gulp
