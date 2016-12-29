010d0da4199666968a3e7f7735a9721bd0b884fd
upliu <i@upliu.net>
2016-12-29 09:19:28 +0800

    Feed 功能完成度 40%

--- views/topic/index.php
+++ views/topic/index.php
@@ -11,3 +11,31 @@
 <button class="btn btn-warning">按钮</button>
 <button class="btn btn-danger">按钮</button>
 </div>
+
+<form>
+    <a type="submit" class="btn btn-primary" disabled>提交</a>
+</form>
+
+<!-- Button trigger modal -->
+<a type="button" class="btn btn-primary btn-lg disabled" data-toggle="modal" data-target="#myModal">
+    Launch demo modal
+</a>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
